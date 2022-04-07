af5f2cb875fe936d3e882f6e5283a6076cb2fefe
Alexander-Aparicio <alexdel2022@gmail.com>
2022-04-07 10:59:42 -0500

    cambio de title del head

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,14 @@ import FrequentQuestions from '../components/Home/FrequentQuestions';
 export default function Home() {
   return (<>
     <Head>
-      <title>Revolution Dance</title>
+      <title>Revolution Dance Studio</title>
       <meta name="Description" content="🤗 Academia de Baile Revolution. Aprende a bailar las coreografías de K-Pop de tus grupos favoritos." />
       {/* <meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34="/> */}
       <link rel="canonical" href="http://www.revolutiondancestudioperu.com/"></link>
       <meta property="og:url" content="http://www.revolutiondancestudioperu.com/"></meta>
       <meta property="og:type" content="website"></meta>
       <meta property="og:title" content="Academia de baile Revolution - Kpop"></meta>
-      <meta property="og:description" content="🤗 Aprende a bailar las mejores coregrafías de tus grupos favoritos de K-pop."></meta>
+      <meta property="og:description" content="🤗 Aprende a bailar las mejores coreografías de tus grupos favoritos de K-pop."></meta>
       <meta property="og:site_name" content="Academia de baile Revolution Dance"></meta>
       <meta property="og:image" content="http://www.revolutiondancestudioperu.com/academia.jpg"></meta>
       <meta property="og:image:secure_url" content="http://www.revolutiondancestudioperu.com/academia.jpg"></meta>
@@ -32,7 +32,7 @@ export default function Home() {
       <meta property="og:image:height" content="458"></meta>
       <meta property="og:image:type" content="image/jpg"></meta>
       <meta name="twitter:card" content="summary_large_image"></meta>
-      <meta name="twitter:description" content="🤗 Aprende a bailar las mejores coregrafías de tus grupos favoritos de K-pop."></meta>
+      <meta name="twitter:description" content="🤗 Aprende a bailar las mejores coreografías de tus grupos favoritos de K-pop."></meta>
       <meta name="twitter:title" content="➤Academia de baile Revolution"></meta>
       <meta name="twitter:image" content="http://www.revolutiondancestudioperu.com/academia.jpg"></meta>
 
