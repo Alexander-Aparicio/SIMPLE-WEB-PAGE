80ba170e47a62a27936be57f8095bfe70fc7b5ad
Alexander-Aparicio <alexdel2022@gmail.com>
2022-12-09 13:26:22 -0500

    add frequent questions

--- components/Home/FrequentQuestions.js
+++ components/Home/FrequentQuestions.js
@@ -34,41 +34,57 @@ line-height: 2;
 `
 
 const FrequentQuestions = ()=>{
-    return(
-        <QuestionsSection>
-            <Title>Preguntas Frecuentes</Title>
+    return (
+      <QuestionsSection>
+        <Title>Preguntas Frecuentes</Title>
 
-            <Title2>¿En cuánto tiempo se puede aprender una coreografía completa de k-pop?</Title2>
-            <Text>
-                Si no tienes mucha experiencia lo podrías lograr en un mes con ensayos de 1h dos veces 
-                por semana o en ensayos de 2h una vez a la semana, ten en cuenta que esto puede variar 
-                dependiendo de tu facilidad para aprender y la complejidad de la coreografía.
-            </Text>
+        <Title2>¿De qué se trata el taller de Baile Moderno?</Title2>
+        <Text>
+          El taller de baile Moderno es una fusión de diversos ritmos que se
+          consideran actuales o en tendencia, el que participe de este taller
+          aprenderá diversas coreografias de la música que está de moda, así
+          mismo podrá desenvolverse en compromisos sociales por medio del baile;
+          desarrollando destreza, habilidad y coordinación para bailar. Entre
+          los ritmos que se practicarán tenemos: Urbano, Funky jazz, tecno,
+          merengue, hip hop (fusión), coreografias de moda.
+        </Text>
 
-            <Title2>¿El género de baile “K-pop” es un estilo único?</Title2>
-            <Text>
-                El género del K-pop es una mezcla de ritmos que va desde lo contemporáneo 
-                hasta el dancehall o estilo urbano, donde los movimientos estilizados, 
-                pero a la vez con exactitud, pueden ser variados y creativos al momento 
-                de ejecutar una coreografía.
-            </Text>
+        <Title2>
+          ¿En cuánto tiempo se puede aprender una coreografía completa de k-pop?
+        </Title2>
+        <Text>
+          Si no tienes mucha experiencia lo podrías lograr en un mes con ensayos
+          de 1h dos veces por semana o en ensayos de 2h una vez a la semana, ten
+          en cuenta que esto puede variar dependiendo de tu facilidad para
+          aprender y la complejidad de la coreografía.
+        </Text>
 
-            <Title2>¿Por qué se considera “cover” a los grupos que bailan K-pop?</Title2>
-            <Text>
-                Por la razón que realizamos un tributo a nuestros grupos favoritos, ya sea de 
-                girlbands o boybands, asimilando las coreografías grupales para que posteriormente 
-                sea presentada en diferentes espacios o escenarios.
-            </Text>
+        <Title2>¿El género de baile “K-pop” es un estilo único?</Title2>
+        <Text>
+          El género del K-pop es una mezcla de ritmos que va desde lo
+          contemporáneo hasta el dancehall o estilo urbano, donde los
+          movimientos estilizados, pero a la vez con exactitud, pueden ser
+          variados y creativos al momento de ejecutar una coreografía.
+        </Text>
 
-            <Title2>¿Cuáles son las ventajas de aprender el K-pop?</Title2>
-            <Text>
-                Practicar el estilo de baile del K-pop, te da agilidad en tus movimientos, capacidad 
-                para adaptarte a cualquier estilo de baile, precisión y estética en tus pasos, 
-                actitud y desenvolvimiento escénico.
-            </Text>
+        <Title2>
+          ¿Por qué se considera “cover” a los grupos que bailan K-pop?
+        </Title2>
+        <Text>
+          Por la razón que realizamos un tributo a nuestros grupos favoritos, ya
+          sea de girlbands o boybands, asimilando las coreografías grupales para
+          que posteriormente sea presentada en diferentes espacios o escenarios.
+        </Text>
 
-        </QuestionsSection>
-    )
+        <Title2>¿Cuáles son las ventajas de aprender el K-pop?</Title2>
+        <Text>
+          Practicar el estilo de baile del K-pop, te da agilidad en tus
+          movimientos, capacidad para adaptarte a cualquier estilo de baile,
+          precisión y estética en tus pasos, actitud y desenvolvimiento
+          escénico.
+        </Text>
+      </QuestionsSection>
+    );
 }
 
 export default FrequentQuestions
